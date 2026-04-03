1da5db44d12b3be844c6245678e718eea5ce1b6e
Matheus Barretto <matheusfbarretto@gmail.com>
2026-04-03 15:24:07 -0300

    edição dos exercicios 24 e 25

--- exercicio_24.js
+++ exercicio_24.js
@@ -6,14 +6,6 @@ const primeiro_numero = Number(prompt('Digite o primeiro número'))
 const segundo_numero = Number(prompt('Digite o segundo número'))
 const terceiro_numero = Number(prompt('Digite o terceiro número'))
 
-let maior_numero = 0
-
-if(primeiro_numero > segundo_numero && primeiro_numero > terceiro_numero){
-    maior_numero = primeiro_numero
-}else if(segundo_numero > primeiro_numero && segundo_numero > terceiro_numero){
-    maior_numero = segundo_numero
-}else{
-    maior_numero = terceiro_numero
-}
+let maior_numero = Math.max(primeiro_numero, segundo_numero, terceiro_numero)
 
 alert(`Maior número = ${maior_numero}`)
--- exercicio_25.js
+++ exercicio_25.js
@@ -6,27 +6,10 @@ const primeiro_numero = Number(prompt('Digite o primeiro número'))
 const segundo_numero = Number(prompt('Digite o segundo número'))
 const terceiro_numero = Number(prompt('Digite o terceiro número'))
 
-let maior_numero = ''
-let menor_numero = ''
+let maior_numero = Math.max(primeiro_numero, segundo_numero, terceiro_numero)
+let menor_numero = Math.min(primeiro_numero, segundo_numero, terceiro_numero)
 
-if(primeiro_numero > segundo_numero && primeiro_numero > terceiro_numero){
-    maior_numero = primeiro_numero
-}else if(segundo_numero > primeiro_numero && segundo_numero > terceiro_numero){
-    maior_numero = segundo_numero
-}else{
-    maior_numero = terceiro_numero
-}
-
-if (primeiro_numero < segundo_numero && primeiro_numero < terceiro_numero){
-    menor_numero = primeiro_numero
-}else if(segundo_numero < primeiro_numero && segundo_numero < terceiro_numero){
-    menor_numero = segundo_numero
-}else{
-    menor_numero = terceiro_numero
-}
 
 alert(`Maior número: ${maior_numero}
 Menor número: ${menor_numero}`)
 
-
-teste 
